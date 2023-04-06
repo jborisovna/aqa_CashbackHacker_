2d60647ab60c3122ae80a9685ff25c03b4126e2f
jenna <ravennasw@gmail.com>
2023-04-06 14:24:18 +0700

    add test testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldRemainIfAmountLessBorder() {
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainIfAmountOverBorder() {
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNotRemainIfAmountEqualBorder() {
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+}
